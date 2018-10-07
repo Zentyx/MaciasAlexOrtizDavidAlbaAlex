c204129ce422cc18991e671c3bc5ef6b814958dd
Zentyx <cmortizdavid@gmail.com>
2018-10-07 11:53:19 +0200

    Update MaciasAlexDavidOrtizAlbaAlex.java

--- src/maciasalexdavidortizalbaalex/MaciasAlexDavidOrtizAlbaAlex.java
+++ src/maciasalexdavidortizalbaalex/MaciasAlexDavidOrtizAlbaAlex.java
@@ -16,7 +16,7 @@ public class MaciasAlexDavidOrtizAlbaAlex {
      */
     public static void main(String[] args) {
         // TODO code application logic herewefsdf
-        7asdasda
+      IESOS
     }
     
 }
